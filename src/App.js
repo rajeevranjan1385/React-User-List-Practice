4e9f93a54916fa5024ed842e11006d11def43f90
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-09-03 23:13:35 +0530

    added cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+
+
 import React from 'react';
+import Card from './components/UI/Card';
 import AddUser from './components/Users/AddUser';
 
 
 function App() {
   return (
-    <div>
-      <AddUser />
-    </div>
+    <AddUser />
   );
 }
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,44 +1,53 @@
 import { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
-const AddUser = props =>{
-    const [username, setUsername] = useState('');
-    const [age, setAge] = useState('');
+const AddUser = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
 
-    const usernameChangeHandler = event =>{
-        if(event.target.value === ''){
-            return false;
-        }
-        setUsername(event.target.value);
+  const usernameChangeHandler = (event) => {
+    if (event.target.value === "") {
+      return false;
     }
+    setUsername(event.target.value);
+  };
 
-    const ageChangeHandler = event =>{
-        if(event.target.value === ''){
-            return false;
-        }
-        setAge(event.target.value);
+  const ageChangeHandler = (event) => {
+    if (event.target.value === "") {
+      return false;
     }
+    setAge(event.target.value);
+  };
 
-    const addUserHandler = event =>{
-        event.reventDefault();
-        if(username === '' || age === ''){
-            return false;
-        }
-        const userInfo = {
-            __id: Math.random(),
-            username: username,
-            age: age
-        }
+  const addUserHandler = (event) => {
+    event.reventDefault();
+    if (username === "" || age === "") {
+      return false;
     }
+    const userInfo = {
+      __id: Math.random(),
+      username: username,
+      age: age,
+    };
+  };
 
-    return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" value={username} onChange={usernameChangeHandler}/>
-            <label htmlFor="age">Age (Years)</label>
-            <input type="number" id="age" value={age} onChange={ageChangeHandler}/>
-            <button type='submit'>Add User</button>
-        </form>
-    );
-}
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" value={age} onChange={ageChangeHandler} />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
 
 export default AddUser;
